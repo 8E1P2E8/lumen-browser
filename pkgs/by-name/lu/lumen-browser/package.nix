a6f83ee02a1905549eca249836bdcd483d9e1474
Your Name <you@example.com>
2026-09-01 07:27:33 -0700

    fix: stage source to user cache directory to allow runtime writes

--- pkgs/by-name/lu/lumen-browser/package.nix
+++ pkgs/by-name/lu/lumen-browser/package.nix
@@ -1,5 +1,5 @@
 { lib
-, stdenv
+, buildNpmPackage
 , fetchFromGitHub
 , makeWrapper
 , nodejs
@@ -7,7 +7,7 @@
 , kubo
 }:
 
-stdenv.mkDerivation rec {
+buildNpmPackage rec {
   pname = "lumen-browser";
   version = "0.9.9";
 
@@ -18,21 +18,29 @@ stdenv.mkDerivation rec {
     hash = "sha256-soVW0Wj5Jf/GUoUc5xzGC2OROacChRMj0FR9dzqqjwk=";
   };
 
+  npmDepsHash = "sha256-OtwQkkGzbqC9Z4qgg5A9xfFUoOsbjr7t2wGsZnNsCNY=";
+  npmFlags = [ "--ignore-scripts" ];
+
   nativeBuildInputs = [ makeWrapper ];
 
-  dontBuild = true;
-  dontConfigure = true;
-
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/share/${pname} $out/bin
     cp -r . $out/share/${pname}
 
     makeWrapper ${nodejs}/bin/npm $out/bin/lumen-browser \
-      --run "cd $out/share/${pname} && [ -d node_modules ] || ${nodejs}/bin/npm install --offline --no-fund || ${nodejs}/bin/npm install" \
-      --set ELECTRON_OVERRIDE_DIST_PATH "${electron}/bin" \
+      --run '
+        RUNTIME_DIR="''${XDG_CACHE_HOME:-$HOME/.cache}/lumen-browser-dev"
+        mkdir -p "$RUNTIME_DIR"
+        cp -r --no-preserve=mode ''$out/share/${pname}/* "$RUNTIME_DIR/"
+        cd "$RUNTIME_DIR"
+      ' \
       --prefix PATH : "${lib.makeBinPath [ nodejs kubo electron ]}" \
       --add-flags "run dev" \
       --add-flags "\$@"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
