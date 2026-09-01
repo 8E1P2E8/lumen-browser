255d6b672140c7992d53d5587ba7011e35f31c4e
Your Name <you@example.com>
2026-09-01 07:13:59 -0700

    fix: export default package and app in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,30 @@
 {
-  description = "Lumen Browser";
+  description = "Lumen Browser Nix Flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.callPackage ./pkgs/by-name/lu/lumen-browser/package.nix {};
-        });
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+      };
+    in {
+      overlays.default = final: prev: {
+        lumen-browser = final.callPackage ./pkgs/by-name/lu/lumen-browser/package.nix {};
+      };
+
+      packages.${system} = {
+        default = pkgs.lumen-browser;
+        lumen-browser = pkgs.lumen-browser;
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${pkgs.lumen-browser}/bin/lumen-browser";
+      };
     };
 }
